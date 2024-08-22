afe98682c77f97d3a3c0fe2aa93b2062093f1bb4
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2024-08-22 17:17:11 +0200

    feat: compute values

--- src/compute.ts
+++ src/compute.ts
@@ -1,7 +1,19 @@
+import { formatUnits } from '@ethersproject/units';
 // @ts-ignore
 import { register } from '@snapshot-labs/checkpoint/dist/src/register';
+import snapshotjs from '@snapshot-labs/snapshot.js';
 import { Mutex } from 'async-mutex';
-import { Governance } from '../.checkpoint/models';
+import { SCORE_API_URL } from './constants';
+import { getSpace } from './hub';
+import { Delegate, Governance } from '../.checkpoint/models';
+
+const DECIMALS = 18;
+const DELEGATION_STRATEGIES = [
+  'delegation',
+  'erc20-balance-of-delegation',
+  'delegation-with-cap',
+  'delegation-with-overrides'
+];
 
 const mutex = new Mutex();
 
@@ -18,16 +30,84 @@ export async function compute(governances: string[]) {
   try {
     register.setCurrentBlock(register.getCurrentBlock() + 1n);
 
-    console.log('compute governances for', governances);
-
     for (const governance of governances) {
+      console.log('computing', governance);
+
+      const space = await getSpace(governance);
+      const delegations = await snapshotjs.utils.getDelegatesBySpace(
+        space.network,
+        governance,
+        'latest'
+      );
+
+      const delegatorCounter = {};
+      for (const delegation of delegations) {
+        if (!delegatorCounter[delegation.delegate]) {
+          delegatorCounter[delegation.delegate] = 0;
+        }
+
+        delegatorCounter[delegation.delegate] += 1;
+      }
+
+      const delegationsMap = Object.fromEntries(
+        delegations.map(d => [d.delegate, d])
+      );
+      const delegatesAddresses = Object.keys(delegationsMap);
+      const uniqueDelegates = Object.values(delegationsMap);
+
+      const strategies = space.strategies.filter(strategy =>
+        DELEGATION_STRATEGIES.includes(strategy.name)
+      );
+
+      const scores = await snapshotjs.utils.getScores(
+        governance,
+        strategies,
+        space.network,
+        delegatesAddresses,
+        'latest',
+        `${SCORE_API_URL}/api/scores`
+      );
+
+      const delegates = uniqueDelegates.map(delegate => ({
+        ...delegate,
+        score: BigInt(
+          Math.floor((scores[0][delegate.delegate] ?? 0) * 10 ** DECIMALS)
+        )
+      }));
+
+      const sortedDelegates = delegates
+        .filter(delegate => delegate.score > 0n)
+        .sort((a, b) => (b.score > a.score ? 1 : -1));
+
+      const totalVotes = sortedDelegates.reduce(
+        (acc, delegate) => acc + delegate.score,
+        0n
+      );
+
       const governanceEntity = new Governance(governance);
-      governanceEntity.currentDelegates = 0;
-      governanceEntity.totalDelegates = 0;
-      governanceEntity.delegatedVotesRaw = '0';
-      governanceEntity.delegatedVotes = '0';
+      governanceEntity.currentDelegates = uniqueDelegates.length;
+      governanceEntity.totalDelegates = uniqueDelegates.length;
+      governanceEntity.delegatedVotesRaw = totalVotes.toString();
+      governanceEntity.delegatedVotes = formatUnits(totalVotes, DECIMALS);
       await governanceEntity.save();
+
+      for (const delegate of sortedDelegates) {
+        const delegateEntity = new Delegate(
+          `${governance}/${delegate.delegate}`
+        );
+        delegateEntity.governance = governance;
+        delegateEntity.user = delegate.delegate;
+        delegateEntity.delegatedVotesRaw = delegate.score.toString();
+        delegateEntity.delegatedVotes = formatUnits(delegate.score, DECIMALS);
+        delegateEntity.tokenHoldersRepresentedAmount =
+          delegatorCounter[delegate.delegate];
+        await delegateEntity.save();
+      }
+
+      console.log('finished compute', governance);
     }
+  } catch (e) {
+    console.error('compute error', e);
   } finally {
     release();
   }
---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const HUB_URL = process.env.HUB_URL ?? 'https://hub.snapshot.org';
+export const SCORE_API_URL =
+  process.env.SCORE_API_URL ?? 'https://score.snapshot.org';
